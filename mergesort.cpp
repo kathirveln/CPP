b6beb0349d429b05427751d456a429215de82b69
Kathirvel Nagaraj <kathirveltce@gmail.com>
2019-01-19 10:32:58 +0530

    check purpose

--- mergesort.cpp
+++ mergesort.cpp
@@ -60,7 +60,7 @@ void merge(int arr[], int l, int m, int r)
  
 /* l is for left index and r is right index of the
    sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
+void mergeSort(int arr[], int l, int r)// for test purpose
 {
     if (l < r)
     {
